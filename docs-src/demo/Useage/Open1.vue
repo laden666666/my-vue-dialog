4164a6627c1d651e64087671319b8f60d5c793eb
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2019-02-14 22:32:23 +0800

    升级my-vue-doc

---
+++ docs-src/demo/Useage/Open1.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="button" @click="open">打开</button>
+</template>
+<script>
+
+let component = {
+    template: '<div class="dialog-content">用class类及装饰器做对话框内容</div>'
+}
+
+export default {
+    methods: {
+        open(){
+            this.$MyDialog.open({
+                content: component,    
+            })
+        }
+    }
+}
+</script>
---
+++ docs-src/demo/Useage/Open4.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="button" @click="open">打开</button>
+</template>
+<script>
+import Vue from 'vue'
+
+let component = {
+    template: '<div class="dialog-content">用Vue.extend创建的子类做componentOptions</div>'
+}
+let component2 = Vue.extend(component)
+
+export default {
+    methods: {
+        open(){
+            this.$MyDialog.open({
+                content: component2 
+            })
+        }
+    }
+}
+</script>
